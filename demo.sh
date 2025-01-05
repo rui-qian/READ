423afa55279cd038edbe88578541432dc9560708
Rui Qian <43597620+rui-qian@users.noreply.github.com>
2025-01-05 15:35:00 +0800

    Delete demo.sh

--- demo.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3 
-python demo.py \
-	--pretrained_model_path="../dataset/SESAME-LLaVA-v1.5-7B" \
-	--vision_tower="../dataset/clip-vit-large-patch14-336" \
-	--model_max_length=2048
