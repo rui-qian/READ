d0c2d82b1ccfa02844430c2dc70fed955d9384b9
rui-qian <qianruii@126.com>
2025-01-21 01:02:35 +0800

    uncomment to eval for ReasonSeg test set with 13B model

--- val_read.sh
+++ val_read.sh
@@ -1,17 +1,28 @@
 # # uncomment to eval for ReasonSeg val set
-# deepspeed --master_port=24996 --include "localhost:0" train_read.py \
-#        --version="./READ-LLaVA-v1.5-7B-for-ReasonSeg-valset" \
-#        --dataset_dir='../dataset' \
-#        --vision_pretrained="../dataset/sam_vit_h_4b8939.pth" \
-#        --eval_only \
-#        --vision_tower="../dataset/clip-vit-large-patch14-336" \
-#        --model_max_length=2048 \
-#        --val_dataset="ReasonSeg" \
-#        --val_split="val" 
+#deepspeed --master_port=24996 --include "localhost:0" train_read.py \
+#       --version="./READ-LLaVA-v1.5-7B-for-ReasonSeg-valset" \
+#       --dataset_dir='../dataset' \
+#       --vision_pretrained="../dataset/sam_vit_h_4b8939.pth" \
+#       --eval_only \
+#       --vision_tower="../dataset/clip-vit-large-patch14-336" \
+#       --model_max_length=2048 \
+#       --val_dataset="ReasonSeg" \
+#       --val_split="val" 
 
 # uncomment to eval for ReasonSeg test set
+#deepspeed --master_port=24996 --include "localhost:0" train_read.py \
+#       --version="./READ-LLaVA-v1.5-7B-for-ReasonSeg-testset" \
+#       --dataset_dir='../dataset' \
+#       --vision_pretrained="../dataset/sam_vit_h_4b8939.pth" \
+#       --eval_only \
+#       --vision_tower="../dataset/clip-vit-large-patch14-336" \
+#       --model_max_length=2048 \
+#       --val_dataset="ReasonSeg" \
+#       --val_split="test"
+
+# uncomment to eval for ReasonSeg test set with 13B model
 deepspeed --master_port=24996 --include "localhost:0" train_read.py \
-       --version="./READ-LLaVA-v1.5-7B-for-ReasonSeg-testset" \
+       --version="./READ-LLaVA-v1.5-13B-for-ReasonSeg-testset" \
        --dataset_dir='../dataset' \
        --vision_pretrained="../dataset/sam_vit_h_4b8939.pth" \
        --eval_only \
